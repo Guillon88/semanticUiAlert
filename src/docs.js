fd73a8773816cbd1cb5658699882343998a21e47
Aitthi <diw_112@hotmail.com>
2016-08-25 19:17:48 +0700

    Update docs.js

--- src/docs.js
+++ src/docs.js
@@ -105,7 +105,7 @@ semantic.ready = function() {
       $.api({
         debug : false,
         on    : 'now',
-        url   : '',
+        url   : 'metadata.json',
         cache : 'local',
         onSuccess: function(response) {
           metadata = response;
